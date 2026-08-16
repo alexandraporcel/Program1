938a589e7a04bddb611ab87abcdf328f639e23ba
Lale <porcelalexandra27@gmail.com>
2026-08-16 17:05:25 -0400

    Add files via upload

---
+++ clase1/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut x = 5;
+    let y = 7;
+    println!("Hola choquito");
+    let suma = x + y;
+    println!("El valor de x es: {}, y el valor de y es: {}", x, y);
+    println!("La suma de x + y es: {}", suma);
+
+    x = 9;
+    println!("El valor de x ahora es: {}", x);
+}
